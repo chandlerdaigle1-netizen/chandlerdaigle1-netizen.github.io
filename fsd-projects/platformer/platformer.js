097f3b439996960bd7c2597d81d0e29dcc89e91f
chandlerdaigle1-netizen <chandler.daigle1@jpschools.org>
2026-02-24 21:24:24 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,31 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+//toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+createPlatform(300, 640, 150, 15, "navy")
+createPlatform(500, 520, 150, 15, "navy")
+createPlatform(120, 400, 245, 15, "navy")
+createPlatform(505, 290, 200, 15, "navy")
+createPlatform(820, 450, 300, 15, "navy")
+createPlatform(760, 160, 650, 15, "navy")
+createPlatform(950, 675, 430, 100, "navy")
 
 
 
     // TODO 3 - Create Collectables
+createCollectable("database", 1245, 550, 0.5, 0.2)
+createCollectable("database", 170, 250, 0.5, 0.2)
+createCollectable("database", 1225, 1, 0.5, 0.2)
 
 
 
-    
     // TODO 4 - Create Cannons
-
+createCannon("right", 620, 2000)
+createCannon("bottom", 850, 5000)
+createCannon("top", 250, 3500)
 
     
     
